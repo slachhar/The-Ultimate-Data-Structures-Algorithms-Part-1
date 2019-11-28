30ec7f306bae03e226d9b1c7430a2b6139f8d2eb
shefali <shefali.lachhar@gmail.com>
2019-11-28 15:08:46 +0530

    Linked List in Java

---
+++ The Ultimate Data Structures and Algorithms-Java/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args){
+        LinkedList list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addFirst(5);
+        System.out.println(list);
+    }
+}
